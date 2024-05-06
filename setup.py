0e93b1ee4a25985da253bc9e5df218d3cf287f72
adrian-kriegel <ak@adriankriegel.com>
2024-05-06 19:35:45 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='odysseus',
+   version='0.0.1',
+   description='',
+   author='Adrian Kriegel',
+   author_email='foo@example.com',
+   packages=['odysseus'], 
+   install_requires=['symengine', 'sympy', 'casadi']
+)
