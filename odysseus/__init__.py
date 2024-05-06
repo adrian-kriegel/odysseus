42fbe99723b91e01812725353f9029f17b0f5197
adrian-kriegel <ak@adriankriegel.com>
2024-05-06 19:35:22 +0200

    merging modules

--- odysseus/__init__.py
+++ odysseus/__init__.py
@@ -1,6 +1,8 @@
 
 from odysseus.link_model import *
-from odysseus.urdf import URDFModel
+from odysseus.urdf import *
 import odysseus.sym_util
 import odysseus.simplify
 import odysseus.save
+from odysseus.symengine2casadi import *
+from odysseus.link_model_simplify import *
