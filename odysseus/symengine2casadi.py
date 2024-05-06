4ae296a49a95252539f120ab273b6730bc95bf33
adrian-kriegel <ak@adriankriegel.com>
2024-05-06 19:32:49 +0200

    handling matrices in symengine2casadi

--- odysseus/symengine2casadi.py
+++ odysseus/symengine2casadi.py
@@ -11,6 +11,12 @@ def to_casadi(expr : se.Expr, subs : dict):
     if expr in subs:
         return subs[expr]
 
+    elif isinstance(expr, se.Matrix):
+
+        ca_exprs = [to_casadi(elem, subs) for elem in expr]
+        
+        return ca.vertcat(*ca_exprs).reshape((expr.rows, expr.cols))
+
     elif isinstance(expr, se.Add):
 
         res = 0
@@ -46,5 +52,10 @@ def to_casadi(expr : se.Expr, subs : dict):
     elif isinstance(expr, se.Number):
 
         return float(expr)
+
+    elif expr is se.pi:
+
+        return ca.pi
+
     
-    raise Exception(f'Cannot convert {type(expr)} to casadi expression: \n{expr}')
+    raise Exception(f'Cannot convert expression of type {type(expr)} to casadi expression: \n{expr}')
